79d5adbefb36daba5ccf6384abf7a44c934a09ee
Billy Russell <billy.hrussell@gmail.com>
2023-04-28 15:51:57 -0700

    Added Users/Posts/VisionBoard models

--- models.py
+++ models.py
@@ -5,3 +5,159 @@ from flask_sqlalchemy import SQLAlchemy
 
 bcrypt = Bcrypt()
 db = SQLAlchemy()
+
+class User(db.Model):
+    """User in the system."""
+
+    __tablename__ = 'users'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    email = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    username = db.Column(
+        db.Text,
+        nullable=False,
+        unique=True,
+    )
+
+    first_name = db.Column(
+        db.Text,
+        nullable=False,
+        unique=False,
+    )
+
+    last_name = db.Column(
+        db.Text,
+        nullable=False,
+        unique=False,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default=DEFAULT_IMAGE_URL,
+    )
+
+    about = db.Column(
+        db.Text,
+    )
+
+    location = db.Column(
+        db.Text,
+    )
+
+    password = db.Column(
+        db.Text,
+        nullable=False,
+    )
+
+    website = db.Column(
+        db.Text,
+        nullable=False,
+        unique=False,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
+
+    # messages = db.relationship('Message', backref="user")
+    # likes = db.relationship('Message', secondary="likes", backref="users_liked")
+    # #backref like_messages
+
+    # followers = db.relationship(
+    #     "User",
+    #     secondary="follows",
+    #     primaryjoin=(Follows.user_being_followed_id == id),
+    #     secondaryjoin=(Follows.user_following_id == id),
+    #     backref="following",
+    # )
+
+    class Post(db.Model):
+    """ ... """
+
+    __tablename__ = 'posts'
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    image_url = db.Column(
+        db.Text,
+        default=DEFAULT_IMAGE_URL,
+    )
+
+    title = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+
+    description = db.Column(
+        db.String(500),
+        nullable=False,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
+
+    original_website_link = db.Column(
+        db.Text,
+        default=DEFAULT_IMAGE_URL,
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete='CASCADE')
+    )
+
+class Vision_Board(db.Model):
+    """Vision Boards"""
+
+    __tablename__ = "vision_boards"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+    )
+
+    title = db.Column(
+        db.String(100),
+        nullable=False,
+    )
+
+    description = db.Column(
+        db.String(500),
+        nullable=False,
+    )
+
+    user_id = db.Column(
+        db.Integer,
+        db.ForeignKey('users.id', ondelete="cascade"),
+        primary_key=True,
+    )
+
+    post_id = db.Column(
+        db.Integer,
+        db.ForeignKey('posts.id'),
+        primary_key=True,
+    )
+
+    timestamp = db.Column(
+        db.DateTime,
+        nullable=False,
+        default=datetime.utcnow,
+    )
+
