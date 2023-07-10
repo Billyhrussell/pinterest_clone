21ecd1d3c346c19a13c51eed4da064ecab7d10f5
Francis Carino <fgcarino@gmail.com>
2023-07-10 12:48:46 -0700

    add id parameter to user serialize function

--- models.py
+++ models.py
@@ -106,6 +106,7 @@ class User(db.Model):
         # serialization is python converting to JSON
         return{
             "username": self.username,
+            "id": self.id,
             "first_name": self.first_name,
             "last_name": self.last_name,
             "email" : self.email,
